71a47c386a84d4ae3137b1977e7fefab976905fd
London2021 <67976260+London2021@users.noreply.github.com>
2020-07-15 09:46:29 -0500

    Egg-Timer Commit Changes

---
+++ Akili_L_ControlFLowExercise.playground/Contents.swift
@@ -0,0 +1,14 @@
+import Cocoa
+
+var timerInseconds = 180
+
+for index in 1...180
+    // the index creates the time elasped
+{ print((index))
+    
+    if index < 180 { print ("Egg is not ready!")}
+    
+        if index == 180 { print ("Egg is ready!")}
+}
+
+ 
